776f33cb98f4c499e28ec73a45e8edf14fae864e
Shubham Jain <shubhamjain2908@gmail.com>
2020-08-22 02:18:46 +0530

    capitalize & project

--- aggregate.js
+++ aggregate.js
@@ -1,5 +1,49 @@
 db.persons.aggregate([
     { $match: { gender: 'female' } },
-    { $group: { _id: { state: "$location.state" }, totalPersons: { $sum: 1 } } },
+    { $group: { _id: { state: '$location.state' }, totalPersons: { $sum: 1 } } },
     { $sort: { totalPersons: -1 } }
 ]).pretty()
+
+
+// Capitalize the first & last name
+db.persons.aggregate([
+    {
+        $project: {
+            _id: 0,
+            gender: 1,
+            fullName: {
+                $concat: [
+                    { $toUpper: { $substrCP: ['$name.first', 0, 1] } },
+                    {
+                        $substrCP: [
+                            '$name.first',
+                            1,
+                            {
+                                $subtract: [
+                                    {
+                                        $strLenCP: '$name.first'
+                                    },
+                                    1
+                                ]
+                            }]
+                    },
+                    ' ',
+                    { $toUpper: { $substrCP: ['$name.last', 0, 1] } },
+                    {
+                        $substrCP: [
+                            '$name.last',
+                            1,
+                            {
+                                $subtract: [
+                                    {
+                                        $strLenCP: '$name.last'
+                                    },
+                                    1
+                                ]
+                            }]
+                    },
+                ]
+            }
+        }
+    }
+])
