2e8c61c2cc753352d103d497818323139202574f
Shubham Jain <shubhamjain2908@gmail.com>
2020-08-22 02:32:32 +0530

    location into geoJSON object: converting string into double

--- aggregate.js
+++ aggregate.js
@@ -47,3 +47,71 @@ db.persons.aggregate([
         }
     }
 ])
+
+// Turning the location into geoJSON object
+db.persons.aggregate([
+    {
+        $project: {
+            _id: 0, name: 1, email: 1, location: {
+                type: 'Point', coordinates: [
+                    {
+                        $convert: {
+                            input: '$location.coordinates.longitude',
+                            to: 'double',
+                            onError: 0.00,
+                            onNull: 0.00
+                        }
+                    },
+                    {
+                        $convert: {
+                            input: '$location.coordinates.latitude',
+                            to: 'double',
+                            onError: 0.00,
+                            onNull: 0.00
+                        }
+                    }
+                ]
+            }
+        }
+    },
+    {
+        $project: {
+            gender: 1,
+            email: 1,
+            location: 1,
+            fullName: {
+                $concat: [
+                    { $toUpper: { $substrCP: ["$name.first", 0, 1] } },
+                    {
+                        $substrCP: [
+                            "$name.first",
+                            1,
+                            {
+                                $subtract: [
+                                    {
+                                        $strLenCP: "$name.first"
+                                    },
+                                    1
+                                ]
+                            }]
+                    },
+                    " ",
+                    { $toUpper: { $substrCP: ["$name.last", 0, 1] } },
+                    {
+                        $substrCP: [
+                            "$name.last",
+                            1,
+                            {
+                                $subtract: [
+                                    {
+                                        $strLenCP: "$name.last"
+                                    },
+                                    1
+                                ]
+                            }]
+                    },
+                ]
+            }
+        }
+    }
+])
