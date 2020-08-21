5add98627ad38096b1269eb70252701b55aefb0d
Shubham Jain <shubhamjain2908@gmail.com>
2020-08-22 03:01:38 +0530

    unwind & addToSet

--- aggregate-arrays.js
+++ aggregate-arrays.js
@@ -1,3 +1,9 @@
 db.friends.aggregate([
     { $group: { _id: { age: '$age' }, allHobbies: { $push: '$hobbies' } } }
 ]).pretty();
+
+// unwind (flattens the elem from arrays)
+db.friends.aggregate([
+    { $unwind: '$hobbies' },
+    { $group: { _id: { age: '$age' }, allHobbies: { $addToSet: '$hobbies' } } }
+]).pretty();
