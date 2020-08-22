f09afb908c1d6ab50cbcdc6950959ed090dca2cb
Shubham Jain <shubhamjain2908@gmail.com>
2020-08-22 21:38:32 +0530

    writing pipeline result into another collections

--- aggregate.js
+++ aggregate.js
@@ -146,3 +146,67 @@ db.persons.aggregate([
     { $skip: 10 },
     { $limit: 10 }
 ]).pretty();
+
+// writing pipeline result into another collections
+db.persons.aggregate([
+    {
+        $project: {
+            _id: 0,
+            name: 1,
+            email: 1,
+            birthdate: {
+                $toDate: '$dob.date'
+            },
+            age: '$dob.age',
+            location: {
+                type: 'Point', coordinates: [
+                    {
+                        $convert: {
+                            input: '$location.coordinates.longitude',
+                            to: 'double',
+                            onError: 0.00,
+                            onNull: 0.00
+                        }
+                    },
+                    {
+                        $convert: {
+                            input: '$location.coordinates.latitude',
+                            to: 'double',
+                            onError: 0.00,
+                            onNull: 0.00
+                        }
+                    }
+                ]
+            }
+        }
+    },
+    {
+        $project: {
+            gender: 1,
+            email: 1,
+            location: 1,
+            birthdate: 1,
+            age: 1,
+            fullName: {
+                $concat: [
+                    { $toUpper: { $substrCP: ["$name.first", 0, 1] } },
+                    {
+                        $substrCP: [
+                            "$name.first", 1, {
+                                $subtract: [{ $strLenCP: "$name.first" }, 1]
+                            }]
+                    },
+                    " ",
+                    { $toUpper: { $substrCP: ["$name.last", 0, 1] } },
+                    {
+                        $substrCP: [
+                            "$name.last", 1, {
+                                $subtract: [{ $strLenCP: "$name.last" }, 1]
+                            }]
+                    },
+                ]
+            }
+        }
+    },
+    { $out: 'transformedPersons' }
+]).pretty()
