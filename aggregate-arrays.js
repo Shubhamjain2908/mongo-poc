248e5f01f3987bed97481f88f87217f563ad2ae4
Shubham Jain <shubhamjain2908@gmail.com>
2020-08-22 20:47:42 +0530

    get highest elem from array

--- aggregate-arrays.js
+++ aggregate-arrays.js
@@ -51,6 +51,8 @@ db.friends.aggregate([
 // get highest elem from array
 db.friends.aggregate([
     { $unwind: '$examScores' },
-    { $project: { _id: 0, name: 1, age: 1, score: "$examScores.score" } },
-    { $sort: { score: -1 } }
+    { $project: { _id: 1, name: 1, age: 1, score: "$examScores.score" } },
+    { $sort: { score: -1 } },
+    { $group: { _id: '$_id', name: { $first: '$name' }, maxScore: { $max: '$score' } } },
+    { $sort: { maxScore: -1 } }
 ]).pretty();
