077b05194707ddd8b67a717d531851fbed71d90d
Shubham Jain <shubhamjain2908@gmail.com>
2020-08-22 20:30:12 +0530

    $filter an array documents

--- aggregate-arrays.js
+++ aggregate-arrays.js
@@ -31,3 +31,19 @@ db.friends.aggregate([
         }
     }
 ]).pretty();
+
+// $filter (input: array which needs to be filtered) // all elrm in sc > 60
+db.friends.aggregate([
+    {
+        $project: {
+            _id: 0,
+            examScores: {
+                $filter: {
+                    input: '$examScores',
+                    as: 'sc',
+                    cond: { $gt: ['$$sc.score', 60] }
+                }
+            }
+        }
+    }
+]).pretty();
